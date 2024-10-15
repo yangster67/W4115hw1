d00831b7feab5169fd2bf73dbc59a6831e703c80
daleyu <yudale2003@gmail.com>
2024-10-15 19:22:38 -0400

    gitignore and starting scaffolding for code

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# W4115hw1
+# Force_pp W4115hw1
 
+### Team:
+Kevin Yang
+Dale Yu
+
+# Homework 1 Components
+
+## Language Components
 KEYWORD = while | if | else | for | forn 
 TYPE = int | vector | ll | pair | 
 SYMBOL = ( | ) | { | } | ;
 OPERATOR = + | - | / | * | = | ==
 NUMBER = [0-9]+
 IDENTIFIER = [a-z][a-z|0-9]*
+
+## Scanning Algorithm
+
---
+++ token/token.h
@@ -0,0 +1,94 @@
+// token.h
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <string>
+#include <unordered_map>
+
+namespace token {
+
+// Define the TokenType enumeration
+enum class TokenType {
+    // Special Tokens
+    ILLEGAL,
+    EOF_T,  // End of File
+
+    // Identifiers + literals
+    IDENTIFIER,  // variable names, function names, etc.
+    NUMBER,      // numeric literals
+
+    // Operators
+    ASSIGN,    // =
+    PLUS,      // +
+    MINUS,     // -
+    SLASH,     // /
+    ASTERISK,  // *
+    EQUAL,     // ==
+    NOT_EQUAL, // !=
+
+    // Delimiters
+    SEMICOLON, // ;
+    COMMA,     // ,
+
+    LPAREN,    // (
+    RPAREN,    // )
+    LBRACE,    // {
+    RBRACE,    // }
+
+    // Keywords
+    WHILE,
+    IF,
+    ELSE,
+    FOR,
+    FORN,
+
+    // Types
+    INT,
+    VECTOR,
+    LL,
+    PAIR,
+
+    // Error Token
+    ERROR
+};
+
+// Token structure
+struct Token {
+    TokenType type;
+    std::string literal;
+
+    Token(TokenType type = TokenType::ILLEGAL, const std::string& literal = "")
+        : type(type), literal(literal) {}
+};
+
+// Function to get the keywords map
+inline const std::unordered_map<std::string, TokenType>& get_keywords() {
+    static const std::unordered_map<std::string, TokenType> keywords = {
+        {"while",  TokenType::WHILE},
+        {"if",     TokenType::IF},
+        {"else",   TokenType::ELSE},
+        {"for",    TokenType::FOR},
+        {"forn",   TokenType::FORN},
+        {"int",    TokenType::INT},
+        {"vector", TokenType::VECTOR},
+        {"ll",     TokenType::LL},
+        {"pair",   TokenType::PAIR},
+        // Add other keywords if necessary
+    };
+    return keywords;
+}
+
+// Function to lookup identifiers and keywords
+inline TokenType LookupIdent(const std::string& ident) {
+    const auto& keywords = get_keywords();
+    auto it = keywords.find(ident);
+    if (it != keywords.end()) {
+        return it->second;
+    }
+    return TokenType::IDENTIFIER;
+}
+
+} // namespace token
+
+#endif // TOKEN_H
