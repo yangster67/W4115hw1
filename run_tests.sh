2979158e800940a605ffec1ef9cd4cf0902306ed
yangster67 <kyang6671@gmail.com>
2024-10-17 19:28:54 -0400

    run tesets change

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ make run_tests
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Running tests..."
     echo "----------------------------------------"
-    ./lexer_tests
+    ./run_tests
     echo "----------------------------------------"
 else
     echo "Compilation failed."
