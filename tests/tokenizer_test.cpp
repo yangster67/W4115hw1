cdd15a84f0af06057ebf02561873f4325ce4be5f
daleyu <yudale2003@gmail.com>
2024-10-17 16:33:01 -0400

    fix tests for lexer

--- tests/tokenizer_test.cpp
+++ tests/tokenizer_test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cctype>
 #include <cassert>
+#include "../lexer/lexer.h"
+#include "../token/token.h"
 
 void test_keyword();
 void test_type();
@@ -12,7 +13,7 @@ void test_number();
 void test_identifier();
 
 int main() {
-    //main testing function
+    // Main testing function
     test_keyword();
     test_type();
     test_symbol();
@@ -24,71 +25,77 @@ int main() {
 }
 
 void test_keyword() {
-    // std::vector<std::string> keywords = {"while", "if", "else", "for", "forn"};
-    // for (const auto& kw : keywords) {
-    //     auto tokens = scanner.scan(kw);
-    //     assert(tokens.size() == 1);
-    //     assert(tokens[0].type == KEYWORD);
-    //     assert(tokens[0].value == kw);
-    // }
+    std::vector<std::string> keywords = {"while", "if", "else", "for", "forn"};
+    for (const auto& kw : keywords) {
+        Lexer lexer(kw);
+        Token token = lexer.nextToken();
+        assert(token.type == LookupIdent(kw));
+        assert(token.literal == kw);
+    }
     std::cout << "test_keyword passed" << std::endl;
 }
 
 void test_type() {
-    // std::vector<std::string> types = {"int", "vector", "ll", "pair"};
-    // for (const auto& ty : types) {
-    //     auto tokens = scanner.scan(ty);
-    //     assert(tokens.size() == 1);
-    //     assert(tokens[0].type == TYPE);
-    //     assert(tokens[0].value == ty);
-    // }
+    std::vector<std::string> types = {"int", "vector", "ll", "pair"};
+    for (const auto& ty : types) {
+        Lexer lexer(ty);
+        Token token = lexer.nextToken();
+        assert(token.type == LookupIdent(ty));
+        assert(token.literal == ty);
+    }
     std::cout << "test_type passed" << std::endl;
 }
 
 void test_symbol() {
-    // Scanner scanner;
-    // std::vector<std::string> symbols = {"(", ")", "{", "}", ";"};
-    // for (const auto& sym : symbols) {
-    //     auto tokens = scanner.scan(sym);
-    //     assert(tokens.size() == 1);
-    //     assert(tokens[0].type == SYMBOL);
-    //     assert(tokens[0].value == sym);
-    // }
+    std::vector<std::string> symbols = {"(", ")", "{", "}", ";"};
+    std::unordered_map<std::string, TokenType> symbolMap{
+        {"(", TokenType::LPAREN},  {")", TokenType::RPAREN},
+        {"{", TokenType::LBRACE},  {"}", TokenType::RBRACE},
+        {";", TokenType::SEMICOLON}
+    };
+    for (const auto& sym : symbols) {
+        Lexer lexer(sym);
+        Token token = lexer.nextToken();
+        assert(token.type == symbolMap[sym]);
+        assert(token.literal == sym);
+    }
     std::cout << "test_symbol passed" << std::endl;
 }
 
 void test_operator() {
-    // Scanner scanner;
-    // std::vector<std::string> operators = {"+", "-", "/", "*", "=", "=="};
-    // for (const auto& op : operators) {
-    //     auto tokens = scanner.scan(op);
-    //     assert(tokens.size() == 1);
-    //     assert(tokens[0].type == OPERATOR);
-    //     assert(tokens[0].value == op);
-    // }
+    std::vector<std::string> operators = {"+", "-", "/", "*", "=", "=="};
+    std::unordered_map<std::string, TokenType> opMap{
+        {"+", TokenType::PLUS},    {"-", TokenType::MINUS},
+        {"/", TokenType::SLASH},   {"*", TokenType::ASTERISK},
+        {"=", TokenType::ASSIGN},  {"==", TokenType::EQUAL}
+    };
+    for (const auto& op : operators) {
+        Lexer lexer(op);
+        Token token = lexer.nextToken();
+        assert(token.type == opMap[op]);
+        assert(token.literal == op);
+    }
     std::cout << "test_operator passed" << std::endl;
 }
 
 void test_number() {
-    // Scanner scanner;
-    // std::vector<std::string> numbers = {"0", "123", "456789"};
-    // for (const auto& num : numbers) {
-    //     auto tokens = scanner.scan(num);
-    //     assert(tokens.size() == 1);
-    //     assert(tokens[0].type == NUMBER);
-    //     assert(tokens[0].value == num);
-    // }
+    std::vector<std::string> numbers = {"0", "123", "456789"};
+    for (const auto& num : numbers) {
+        Lexer lexer(num);
+        Token token = lexer.nextToken();
+        assert(token.type == TokenType::NUMBER);
+        assert(token.literal == num);
+    }
     std::cout << "test_number passed" << std::endl;
 }
 
 void test_identifier() {
-    // Scanner scanner;
-    // std::vector<std::string> identifiers = {"a", "abc", "a1b2c3", "variable", "x0"};
-    // for (const auto& id : identifiers) {
-    //     auto tokens = scanner.scan(id);
-    //     assert(tokens.size() == 1);
-    //     assert(tokens[0].type == IDENTIFIER);
-    //     assert(tokens[0].value == id);
-    // }
+    std::vector<std::string> identifiers = {"a", "abc", "a1b2c3", "variable", "x0"};
+    for (const auto& id : identifiers) {
+        Lexer lexer(id);
+        Token token = lexer.nextToken();
+        assert(token.type == TokenType::IDENTIFIER);
+        assert(token.literal == id);
+    }
     std::cout << "test_identifier passed" << std::endl;
 }
